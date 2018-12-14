119f929a707e36428eb185bc5840d644eb2285cd
ShangYiguoguo <2759191887@qq.com>
2018-12-14 11:38:51 +0800

    不知道改了啥

--- SupermarketOnlineStatic/src/js/test/MyTestCtrl.js
+++ SupermarketOnlineStatic/src/js/test/MyTestCtrl.js
@@ -1,18 +1,20 @@
-(function() {
+(function () {
   var ctrls = angular.module('controllers');
-  ctrls.controller('TsetMyTestCtrl', ['$scope', '$log','DialogService','DataService','MyUtilService', TsetMyTestCtrl]);
+  ctrls.controller('TsetMyTestCtrl', ['$scope', '$log', 'DialogService', 'DataService', 'MyUtilService', TsetMyTestCtrl]);
 
-  function TsetMyTestCtrl($scope, $log,DialogService,DataService,MyUtilService) {
+  function TsetMyTestCtrl($scope, $log, DialogService, DataService, MyUtilService) {
     $log.debug('TsetMyTestCtrl init...');
 
     // 处理scope销毁
-    $scope.$on('$destroy', function() {
+    $scope.$on('$destroy', function () {
       $log.debug('TsetMyTestCtrl destroy...');
     });
     DialogService.showWait('获取后台数据中');
-    DataService.send('/',{echo:new Date().getTime()},function(data){
+    DataService.send('/', {
+      echo: new Date().getTime()
+    }, function (data) {
       DialogService.hideWait();
-      $scope.info=MyUtilService.trustAsHtml(
+      $scope.info = MyUtilService.trustAsHtml(
         MyUtilService.jsonSyntaxHighlight(
           MyUtilService.formatJson(JSON.stringify(data))));
     });
